c434e624d9f3690a05bede17754b4874ae3ba5ee
Mark Lee <mark.lee@capricasoftware.co.uk>
2020-04-22 10:00:02 +0100

    Bump to Java 11 and Java Modules

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module uk.co.caprica.vlcj.file.filters {
+    requires java.desktop;
+    exports uk.co.caprica.vlcj.filefilters;
+    exports uk.co.caprica.vlcj.filefilters.filetypes;
+    exports uk.co.caprica.vlcj.filefilters.swing;
+}
